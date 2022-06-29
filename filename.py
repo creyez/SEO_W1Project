b9cb7bd63b73ed425a2f4ff31eb34ace59c98291
Christian Reyes <52749213+creyez@users.noreply.github.com>
2022-06-29 19:54:40 -0400

    Update filename.py

--- filename.py
+++ filename.py
@@ -1,1 +1,10 @@
+import requests
 
+
+url = "https://na1.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/PLATINUM/IV?page=1&api_key=RGAPI-5e9e49a2-a955-4ee5-b714-e9ac920fc59a"
+#url for League of Legends players that are ranked PLATNIUM IV
+#https://developer.riotgames.com/apis#league-exp-v4/GET_getLeagueEntries
+#update link every 24 hours because Development API Key resets
+
+response = requests.get(url)
+print(response.json())
