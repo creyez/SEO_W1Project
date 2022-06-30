86a5b52af13bb89ae9379147c6a4c53f0b273307
Christian Reyes <52749213+creyez@users.noreply.github.com>
2022-06-29 20:54:46 -0400

    Update filename.py

--- filename.py
+++ filename.py
@@ -3,10 +3,13 @@ import json
 import pandas as pd
 import sqlalchemy as db
 
-url = "https://na1.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/PLATINUM/IV?page=1&api_key=RGAPI-5e9e49a2-a955-4ee5-b714-e9ac920fc59a"
-#url for League of Legends players that are ranked PLATNIUM IV
-#https://developer.riotgames.com/apis#league-exp-v4/GET_getLeagueEntries
-#update link every 24 hours because Development API Key resets
+url = "https://na1.api.riotgames.com/lol/league-exp/v4/entries/ \
+       RANKED_SOLO_5x5/PLATINUM/IV?page=1&\
+       api_key=RGAPI-5e9e49a2-a955-4ee5-b714-e9ac920fc59a"
+
+# url for League of Legends players that are ranked PLATNIUM IV
+# https://developer.riotgames.com/apis#league-exp-v4/GET_getLeagueEntries
+# update link every 24 hours because Development API Key resets
 
 response = requests.get(url)
 response = response.json()
@@ -16,11 +19,11 @@ print("Wins: " + str(response[0]["wins"]))
 print("Losses: " + str(response[0]["losses"]))
 
 df = pd.DataFrame.from_dict(response)
-#converts dictionary into a pandas dataframe
+# converts dictionary into a pandas dataframe
 print(df)
 
 engine = db.create_engine('sqlite:///data_base_name.db')
 df.to_sql('platPlayers', con=engine, if_exists='replace', index=False)
 query_result = engine.execute("SELECT * FROM platPlayers;").fetchall()
 print(pd.DataFrame(query_result))
-#converts pandas dataframe to a data base
+# converts pandas dataframe to a data base
