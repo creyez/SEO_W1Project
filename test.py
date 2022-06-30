65c7172e51e49b6a3297613c71b772a5b703b64e
Christian Reyes <52749213+creyez@users.noreply.github.com>
2022-06-30 17:14:46 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import unittest
-from lolWL import function1, function2
+from lolWL import testFuction
 
 
 class TestFileName(unittest.TestCase):
     def test_function1(self):
-        self.assertEqual(1 - 1, 0)
+        self.assertEqual(testFuction(0), 0)
 
-    def test_function2(self):
-        self.assertEqual(2 + 1, 3)
-        self.assertEqual(2.1 + 1.2, 3.3)
+#     def test_function2(self):
+#         self.assertEqual(2 + 1, 3)
+#         self.assertEqual(2.1 + 1.2, 3.3)
 
 if __name__ == '__main__':
     unittest.main()
