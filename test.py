8301c19aae6f235447fce3bdae0394e11f69d87f
Christian Reyes <52749213+creyez@users.noreply.github.com>
2022-06-30 16:53:21 -0400

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class TestFileName(unittest.TestCase):
 
     def test_function2(self):
         self.assertEqual( 2 + 1, 3)
-        self.assertEqual(2.1 + 1.2), 3.3)
+        self.assertEqual(2.1 + 1.2, 3.3)
 
 if __name__ == '__main__':
     unittest.main()
